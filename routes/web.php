eeac4208359da29d7526a6793fc0a29a1cb0ed7a
Evo <evo@Evos-MacBook-Pro.local>
2025-06-28 18:29:33 +0700

    fix: login routig

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Landing Page
+Route::get('/', [AuthenticatedSessionController::class, 'create'])
+        ->name('/');
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
 

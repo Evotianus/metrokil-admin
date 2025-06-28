a38e54695b2815bdbb965582b1752d0b2e19aca9
shackiru <shaquilledputra@gmail.com>
2025-06-28 19:26:33 +0700

    rollback vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,8 +93,21 @@
         <div class="layout-overlay layout-menu-toggle"></div>
     </div>
 
-    {{-- Page JS --}}
+    {{-- Core JS --}}
+    <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('assets/vendor/js/menu.js') }}"></script>
+
+    {{-- Main JS --}}
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+
+    {{-- Page JS --}}Add commentMore actions
     @stack('js')
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="{{ asset('assets/vendor/libs/github/github.min.js') }}"></script>
 </body>
 
 </html>
